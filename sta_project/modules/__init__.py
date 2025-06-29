e084378a199b818cf080dec8120b3fe5d0096973
Blaze906 <106680155+Blaze906@users.noreply.github.com>
2025-06-29 16:37:48 +0200

    Update __init__.py

--- sta_project/modules/__init__.py
+++ sta_project/modules/__init__.py
@@ -1,1 +1,7 @@
 # This file makes 'modules' a Python package.
+# Rende 'modules' un pacchetto importabile da sta.py
+from importlib import import_module
+
+# Re-export dei sotto-moduli per semplicità
+text_analyzer = import_module(".text_analyzer", package=__name__)
+cleaner = import_module(".cleaner", package=__name__)
